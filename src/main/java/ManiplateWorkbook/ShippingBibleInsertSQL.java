0b6b385e981c8f665a81b084b0159758d41e0f1d
jonesal26gc <holly001git>
2017-07-20 18:14:32 +0100

    restructure the SQL extract processing with BUFFERED_WRITER.

--- src/main/java/ManiplateWorkbook/ShippingBibleInsertSQL.java
+++ src/main/java/ManiplateWorkbook/ShippingBibleInsertSQL.java
@@ -5,12 +5,16 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class ShippingBibleInsertSQL {
+    public static final String END_OF_LINE = "\n";
 
     public static void main(String[] args) throws Exception {
         DepotLocationsList depotLocationsList;
         StoreLocationsList storeLocationsList;
         DepotToStoreRouteListsList depotToStoreRouteListsList;
-        PrintWriter printWriter = new PrintWriter("ShippingNetworkInsertSQL.txt");
+
+        File file = new File("ShippingNetworkInsertSQL.txt");
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
         try {
             FileInputStream fileIn = new FileInputStream("ShippingBibleExtract.ser");
@@ -24,109 +28,132 @@ public class ShippingBibleInsertSQL {
             i.printStackTrace();
             return;
         } catch (ClassNotFoundException c) {
-            printWriter.println("class not found");
+            System.out.println("De-serialization of objects failed.");
             c.printStackTrace();
             return;
         }
 
-        processStoreLocations(storeLocationsList, printWriter);
-        processDepotLocations(depotLocationsList, printWriter);
-        processRoutes(depotToStoreRouteListsList, printWriter);
+        processStoreLocations(storeLocationsList, bufferedWriter);
+        processDepotLocations(depotLocationsList, bufferedWriter);
+        processRoutes(depotToStoreRouteListsList, bufferedWriter);
+
+        System.out.println("SQL generation is complete.");
     }
 
-    private static void processStoreLocations(StoreLocationsList storeLocationsList, PrintWriter printWriter) {
+    private static void processStoreLocations(StoreLocationsList storeLocationsList, BufferedWriter bufferedWriter) {
         for (Location location : storeLocationsList.getLocations()) {
-            createSQLforStoreLocation(printWriter, location);
+            createSQLforStoreLocation(bufferedWriter, location);
         }
     }
 
-    private static void processDepotLocations(DepotLocationsList depotLocationsList, PrintWriter printWriter) {
+    private static void processDepotLocations(DepotLocationsList depotLocationsList, BufferedWriter bufferedWriter) {
         for (Location location : depotLocationsList.getLocations()) {
-            createSQLforDepotLocations(printWriter, location);
+            createSQLforDepotLocations(bufferedWriter, location);
         }
     }
 
-    private static void processRoutes(DepotToStoreRouteListsList depotToStoreRouteListsList, PrintWriter printWriter) {
+    private static void processRoutes(DepotToStoreRouteListsList depotToStoreRouteListsList, BufferedWriter bufferedWriter) {
         DateFormat isoDate = new SimpleDateFormat("yyyy-MM-dd");
         int routeNumber = 0;
         for (DepotToStoreRouteList depotToStoreRouteList : depotToStoreRouteListsList.getDepotToStoreRouteLists()) {
             for (DepotToStoreRoute depotToStoreRoute : depotToStoreRouteList.getDepotToStoreRoutes()) {
                 routeNumber++;
-                createSQLforRoute(printWriter, isoDate, routeNumber, depotToStoreRoute);
+                createSQLforRoute(bufferedWriter, isoDate, routeNumber, depotToStoreRoute);
                 if (depotToStoreRoute.getDepotList() == null) {
-                    createSQLforNonTrunkedRouteLeg(printWriter, routeNumber, depotToStoreRoute);
+                    createSQLforNonTrunkedRouteLeg(bufferedWriter, routeNumber, depotToStoreRoute);
                 } else {
-                    createSQLforTrunkedRouteLeg(printWriter, routeNumber, depotToStoreRoute);
+                    createSQLforTrunkedRouteLeg(bufferedWriter, routeNumber, depotToStoreRoute);
                 }
             }
         }
     }
 
-    private static void createSQLforStoreLocation(PrintWriter printWriter, Location location) {
-        printWriter.println("insert into location " +
-                "(location_code,location_type_code,name,format,county,postcode,country,reporting_region) " +
-                "values (" + location.getLocationCode() +
-                ",'" + location.getLocationTypeCode() + "'" +
-                ",'" + location.getName().replaceAll("'", "") + "'" +
-                ",'" + location.getFormat() + "'" +
-                ",'" + location.getCounty() + "'" +
-                ",'" + location.getPostcode() + "'" +
-                ",'" + location.getCountry() + "'" +
-                ",'" + location.getReportingRegion() + "');");
+    private static void createSQLforStoreLocation(BufferedWriter bufferedWriter, Location location) {
+        try {
+            bufferedWriter.write("insert into location " +
+                    "(location_code,location_type_code,name,format,county,postcode,country,reporting_region) " +
+                    "values (" + location.getLocationCode() +
+                    ",'" + location.getLocationTypeCode() + "'" +
+                    ",'" + location.getName().replaceAll("'", "") + "'" +
+                    ",'" + location.getFormat() + "'" +
+                    ",'" + location.getCounty() + "'" +
+                    ",'" + location.getPostcode() + "'" +
+                    ",'" + location.getCountry() + "'" +
+                    ",'" + location.getReportingRegion() + "');" + END_OF_LINE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
-    private static void createSQLforDepotLocations(PrintWriter printWriter, Location location) {
-        printWriter.println("insert into location " +
-                "(location_code,location_type_code,name,format) " +
-                "values (" + location.getLocationCode() +
-                ",'" + location.getLocationTypeCode() + "'" +
-                ",'" + location.getName() + "'" +
-                ",'" + location.getFormat() + "');");
+    private static void createSQLforDepotLocations(BufferedWriter bufferedWriter, Location location) {
+        try {
+            bufferedWriter.write("insert into location " +
+                    "(location_code,location_type_code,name,format) " +
+                    "values (" + location.getLocationCode() +
+                    ",'" + location.getLocationTypeCode() + "'" +
+                    ",'" + location.getName() + "'" +
+                    ",'" + location.getFormat() + "');" + END_OF_LINE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
-    private static void createSQLforRoute(PrintWriter printWriter, DateFormat isoDate, int routeNumber, DepotToStoreRoute depotToStoreRoute) {
-        printWriter.println("insert into route " +
-                "(route_number, location_code_start, location_code_end, route_type_code, start_date, end_date) " +
-                "values (" + String.valueOf(routeNumber) +
-                "," + depotToStoreRoute.getDepot() +
-                "," + depotToStoreRoute.getStore() +
-                ",'" + depotToStoreRoute.getRouteTypeCode() + "'" +
-                ",'" + isoDate.format(depotToStoreRoute.getStartDate()) + "'" +
-                ",'" + isoDate.format(depotToStoreRoute.getEndDate()) + "'" +
-                ");");
+    private static void createSQLforRoute(BufferedWriter bufferedWriter, DateFormat isoDate, int routeNumber, DepotToStoreRoute depotToStoreRoute) {
+        try {
+            bufferedWriter.write("insert into route " +
+                    "(route_number, location_code_start, location_code_end, route_type_code, start_date, end_date) " +
+                    "values (" + String.valueOf(routeNumber) +
+                    "," + depotToStoreRoute.getDepot() +
+                    "," + depotToStoreRoute.getStore() +
+                    ",'" + depotToStoreRoute.getRouteTypeCode() + "'" +
+                    ",'" + isoDate.format(depotToStoreRoute.getStartDate()) + "'" +
+                    ",'" + isoDate.format(depotToStoreRoute.getEndDate()) + "'" +
+                    ");" + END_OF_LINE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
-    private static void createSQLforNonTrunkedRouteLeg(PrintWriter printWriter, int routeNumber, DepotToStoreRoute depotToStoreRoute) {
-        printWriter.println("insert into route_leg " +
-                "(route_number, leg_number, location_code_from, location_code_to) " +
-                "values (" + String.valueOf(routeNumber) +
-                ",1" +
-                "," + depotToStoreRoute.getDepot() +
-                "," + depotToStoreRoute.getStore() +
-                ");");
+    private static void createSQLforNonTrunkedRouteLeg(BufferedWriter bufferedWriter, int routeNumber, DepotToStoreRoute depotToStoreRoute) {
+        try {
+            bufferedWriter.write("insert into route_leg " +
+                    "(route_number, leg_number, location_code_from, location_code_to) " +
+                    "values (" + String.valueOf(routeNumber) +
+                    ",1" +
+                    "," + depotToStoreRoute.getDepot() +
+                    "," + depotToStoreRoute.getStore() +
+                    ");" + END_OF_LINE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
-    private static void createSQLforTrunkedRouteLeg(PrintWriter printWriter, int routeNumber, DepotToStoreRoute depotToStoreRoute) {
-        int legNumber = 0;
-        String fromDepot = depotToStoreRoute.getDepot();
-        for (String depot : depotToStoreRoute.getDepotList()) {
+    private static void createSQLforTrunkedRouteLeg(BufferedWriter bufferedWriter, int routeNumber, DepotToStoreRoute depotToStoreRoute) {
+        try {
+            int legNumber = 0;
+            String fromDepot = depotToStoreRoute.getDepot();
+            for (String depot : depotToStoreRoute.getDepotList()) {
+                legNumber++;
+                bufferedWriter.write("insert into route_leg " +
+                        "(route_number, leg_number, location_code_from, location_code_to) " +
+                        "values (" + String.valueOf(routeNumber) +
+                        "," + String.valueOf(legNumber) +
+                        "," + fromDepot +
+                        "," + depot +
+                        ");" + END_OF_LINE);
+                fromDepot = depot;
+            }
             legNumber++;
-            printWriter.println("insert into route_leg " +
+            bufferedWriter.write("insert into route_leg " +
                     "(route_number, leg_number, location_code_from, location_code_to) " +
                     "values (" + String.valueOf(routeNumber) +
                     "," + String.valueOf(legNumber) +
                     "," + fromDepot +
-                    "," + depot +
-                    ");");
-            fromDepot = depot;
+                    "," + depotToStoreRoute.getStore() +
+                    ");" + END_OF_LINE);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        legNumber++;
-        printWriter.println("insert into route_leg " +
-                "(route_number, leg_number, location_code_from, location_code_to) " +
-                "values (" + String.valueOf(routeNumber) +
-                "," + String.valueOf(legNumber) +
-                "," + fromDepot +
-                "," + depotToStoreRoute.getStore() +
-                ");");
     }
 }
